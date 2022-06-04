c3888626b575072c113dfb30abb8572bdec694be
Rans4ckeR <ransacker@outlook.com>
2022-06-04 17:37:32 +0200

    Update EllipticCurve implementation

--- RS.Schannel.Manager.API/Entities/WindowsApiEllipticCurveConfiguration.cs
+++ RS.Schannel.Manager.API/Entities/WindowsApiEllipticCurveConfiguration.cs
@@ -1,3 +1,13 @@
 ﻿namespace RS.Schannel.Manager.API;
 
-public readonly record struct WindowsApiEllipticCurveConfiguration(uint cbSize, string pszOid, string pwszName, CRYPT_OID_GROUP_ID dwGroupId, uint? dwMagic, ALG_ID algId, ALG_CLASS algClass, ALG_TYPE algType, ALG_SID algSid, uint? dwBitLength, uint? keyBytesLength, CRYPT_OID_FLAG flags, string? pwszCNGAlgid, string? pwszCNGExtraAlgid);
+public readonly record struct WindowsApiEllipticCurveConfiguration(
+    string pszOid,
+    string pwszName,
+    CRYPT_OID_GROUP_ID dwGroupId,
+    uint? dwMagic,
+    CALG algId,
+    uint? dwBitLength,
+    BCRYPT_MAGIC? bcryptMagic,
+    CRYPT_OID_FLAG flags,
+    List<string> CngAlgorithms,
+    string? pwszCNGExtraAlgid);
---
+++ RS.Schannel.Manager.API/Services/IEllipticCurveIdentifierService.cs
@@ -0,0 +1,10 @@
+﻿namespace RS.Schannel.Manager.API;
+
+public interface IEllipticCurveIdentifierService
+{
+    Dictionary<BCRYPT_ECC_CURVE, string> GetEllipticCurveIdentifiers();
+
+    public string GetIdentifier(string code);
+
+    public string GetIdentifier(BCRYPT_ECC_CURVE code);
+}
